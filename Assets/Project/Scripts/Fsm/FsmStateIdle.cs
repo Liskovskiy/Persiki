6774925daec3d136ce9828ec0c7c5c1b6cb4bf29
Liskovskiy <homyak22819@gmail.com>
2025-07-26 02:33:43 +0500

    create fsm try 1

---
+++ Assets/Project/Scripts/Fsm/FsmStateIdle.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace FSM.Scripts
+{
+    public class FsmStateIdle : FsmState
+    {
+        public FsmStateIdle(Fsm fsm) : base(fsm) { }
+
+        public override void Enter()
+        {
+            Debug.Log("Idle state [ENTER]")
+        }
+        public override void Exit()
+        {
+            Debug.Log("Idle state [EXIT]")
+        }
+        public override void Update()
+        {
+            Debug.Log("Idle state UPDATE")
+
+            if (Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
+            {
+                Fsm.SetState<FsmStateWalk>();
+            }
+        }
+
+    }
+}
---
+++ Assets/Project/Scripts/FsmState.cs
@@ -0,0 +1,14 @@
+namespace FSM.Scripts
+
+    public class FsmState
+    {
+        protected readonly Fsm Fsm;
+        
+        public FsmState(Fsm fsm)
+    {
+            Fsm = fsm;
+    }
+        public virtual void Enter() { }
+        public virtual void Exit() { }
+        public virtual void Update() { }
+    }
